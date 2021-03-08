307d5d5901e229099f1d7fb16abcae54c900fb6f
JiHoon <kjhktc@naver.com>
2021-03-08 22:34:12 +0900

    투명 윈도우 소스 추가

--- src/main/java/ui/Window.java
+++ src/main/java/ui/Window.java
@@ -1,84 +1,43 @@
 package ui;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
+// https://banana-media-lab.tistory.com/entry/%ED%88%AC%EB%AA%85%ED%95%9C-JPanel-%EB%A7%8C%EB%93%A4%EA%B8%B0
 public class Window {
 	private final JFrame window = new JFrame();
-	private final int windowWidth = 500;
-	private final int windowHeight = 500;
-
-	public void run() {
-
-		JPanel mainPanel = mainPanel();
-		window.add(mainPanel);
+	private int windowWidth = 500;
+	private int windowHeight = 500;
+	public void initial() {
+		window.setDefaultCloseOperation(javax.swing.WindowConstants.HIDE_ON_CLOSE);
+		window.setUndecorated(true);
+		window.setType(javax.swing.JFrame.Type.UTILITY);
+		window.getRootPane().putClientProperty("apple.awt.draggableWindowBackground", false);
 
 		Dimension dimen = Toolkit.getDefaultToolkit().getScreenSize();
-		window.setBounds(dimen.width / 2 - windowWidth / 2, dimen.height / 2 - windowHeight / 2, 500, 500);
-		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		window.setBounds(dimen.width / 2 - windowWidth/2, dimen.height / 2 - windowHeight/2, windowWidth, windowHeight);
+		window.getContentPane().setLayout(new java.awt.BorderLayout(0, 0));
+		window.setBackground(new Color(0, 0, 0, 0));
+	}
 
-		window.dispose();
-		window.setUndecorated(true);
-		window.setBackground(new Color(1.0f, 1.0f, 1.0f, 0.5f));
-		window.getContentPane().setBackground(new Color(1.0f, 1.0f, 1.0f, 0.5f));
-
+	public void drawString(String sentence, int size, int y_coord, int x_coord, float h, float s, float b) {
+		JLabel key = new JLabel();
+		key.setText(sentence);
+		Font myFont = new Font("고딕", Font.BOLD, size);
+		key.setFont(myFont);
+		key.setForeground(Color.getHSBColor(h, s, b));
+		key.setBorder(BorderFactory.createEmptyBorder(y_coord, x_coord, 0, 0));
+		window.getContentPane().add(key, 0);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
-
-	}
-
-	private JPanel mainPanel() {
-		JPanel jp = new JPanel(new BorderLayout()) {
-			@Override
-			public void paintComponents(Graphics g) {
-				g.setColor(getBackground());
-				Rectangle r = g.getClipBounds();
-				g.fillRect(r.x, r.y, r.width, r.height);
-				super.paintComponent(g);
-			}
-		};
-		JPanel up = upPanel();
-		JPanel down = downPanel();
-		down.setPreferredSize(new Dimension(windowWidth, windowHeight / 10));
-
-		jp.add(up, BorderLayout.CENTER);
-		jp.add(down, BorderLayout.SOUTH);
-		jp.setOpaque(false);
-		return jp;
-	}
-
-	private JPanel upPanel() {
-		JPanel jp = new JPanel(new GridLayout());
-		JLabel jl = new JLabel();
-		jp.add(jl);
-		return jp;
-	}
-
-	private JPanel downPanel() {
-		JPanel jp = new JPanel(new GridLayout());
-		JTextField jt = new JTextField() {
-			@Override
-			public void setBorder(Border border) {
-			}
-
-			@Override
-			protected void paintComponent(Graphics g) {
-				g.setColor(Color.red);
-				g.fillRect(0, 0, jp.getWidth(), jp.getHeight());
-			}
-		};
-
-		jt.addActionListener(e -> {
-			JTextField t = (JTextField) e.getSource();
-			System.out.println(t.getText());
-			t.setText("");
-		});
-		jp.add(jt);
-		return jp;
 	}
 
 
 	public static void main(String[] args) {
-		new Window().run();
+		Window kk = new Window();
+		kk.initial();
+		kk.drawString("강이사 이거 어때?", 15, 0, 0, 0, 0, 0);
 	}
 }
+
+
